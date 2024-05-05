4bf61fb14dba53b3eeb2a195e1ff21d510c0c474
Arandia-AericaMae-BSIT32E1 <arandiaaerica@gmail.com>
2024-05-06 00:40:17 +0800

    added LoginRequest in folder Models

--- PREFINALS_AUTH_ARANDIA_AERICAMAE/AuthServer/obj/Debug/net8.0/AuthServer.AssemblyInfo.cs
+++ PREFINALS_AUTH_ARANDIA_AERICAMAE/AuthServer/obj/Debug/net8.0/AuthServer.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AuthServer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5e371f7a57acb9169397139240b1af89987f5064")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dcf36e8dd2fb29c566ae527e2ae45e6221c8d05e")]
 [assembly: System.Reflection.AssemblyProductAttribute("AuthServer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AuthServer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- PREFINALS_AUTH_ARANDIA_AERICAMAE/obj/Debug/net8.0/PREFINALS_AUTH_ARANDIA_AERICAMAE.AssemblyInfo.cs
+++ PREFINALS_AUTH_ARANDIA_AERICAMAE/obj/Debug/net8.0/PREFINALS_AUTH_ARANDIA_AERICAMAE.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PREFINALS_AUTH_ARANDIA_AERICAMAE")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5e371f7a57acb9169397139240b1af89987f5064")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dcf36e8dd2fb29c566ae527e2ae45e6221c8d05e")]
 [assembly: System.Reflection.AssemblyProductAttribute("PREFINALS_AUTH_ARANDIA_AERICAMAE")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PREFINALS_AUTH_ARANDIA_AERICAMAE")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
