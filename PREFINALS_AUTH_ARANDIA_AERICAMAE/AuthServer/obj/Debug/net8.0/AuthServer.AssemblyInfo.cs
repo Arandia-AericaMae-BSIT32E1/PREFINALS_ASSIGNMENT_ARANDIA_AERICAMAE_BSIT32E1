05e104a641ce07ba83cbceabd87951bbe47bf970
Arandia-AericaMae-BSIT32E1 <arandiaaerica@gmail.com>
2024-05-05 23:56:28 +0800

    install System.IdentityModel.Tokens.Jwt

--- PREFINALS_AUTH_ARANDIA_AERICAMAE/AuthServer/obj/Debug/net8.0/AuthServer.AssemblyInfo.cs
+++ PREFINALS_AUTH_ARANDIA_AERICAMAE/AuthServer/obj/Debug/net8.0/AuthServer.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AuthServer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e3168e88dd76bffeb337ee076a0abf1da76ef231")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fda013c281532915a4b0beffe6d69c9d100d472b")]
 [assembly: System.Reflection.AssemblyProductAttribute("AuthServer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AuthServer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
